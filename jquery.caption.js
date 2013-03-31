e684277f6b73151ef93c91aa601d8f9712ee898a
Eric Magnuson <eric@ericmagnuson.me>
2013-03-31 17:12:52 -0600

    Initial commit

---
+++ jquery.caption.js
@@ -0,0 +1,43 @@
+/*!
+ * caption.js | easily and semantically add captions to your images
+ * http://captionjs.com
+ *
+ * Copyright 2013, Eric Magnuson
+ * Released under the MIT license
+ * https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
+ *
+ * v.0.9.0
+ * Date: 2013-3-31
+ */
+(function($, window, undefined){
+	$.fn.captionjs = function(opts){
+		// Default values for options
+		var defaults = {
+			'class_name'   : 'caption',  // Class name assigned to each <figure>
+			'schema'       : false,		 // User schema.org markup (i.e., itemtype, itemprop)
+			'use_data_attr': false,		 // Get the caption text from a data-caption attribute instead of the alt text
+			'debug_mode'   : false       // Output debug info to the JS console
+		}
+
+		// Extend the options from defaults with user's options
+		var options = $.extend(defaults, opts || {});
+
+		// jQuery chainability
+		return this.each(function(){
+			// Do logic here.
+			if (options.debug_mode) console.log('caption.js | Starting.');
+
+			// Form basic structures and assign vars
+			var $this = $(this);  // The image
+			var $caption = !options.use_data_attr ? $this.attr('alt') : $this.data('caption');
+
+			if (options.debug_mode) console.log('caption.js | Caption: ' + $caption);
+
+			var $figure = $this.wrap('<figure class="' + options.class_name + '"/>').after('<figcaption/>').parent();
+			var $figcaption = $this.next('figcaption').html($caption);
+
+			// Set width of the figure, our top-most container for caption.js.
+			$figure.width($this.outerWidth());
+		});
+	};
+})(jQuery, window);
