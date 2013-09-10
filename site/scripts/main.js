4a0ce20365f09b39c1968a93bd381ca611eb1f35
Eric Magnuson <eric@ericmagnuson.me>
2013-09-10 15:35:10 -0600

    Adding the site to git repo

---
+++ site/scripts/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	SyntaxHighlighter.autoloader(
+			'js jscript javascript  site/scripts/syntaxhighlighter/scripts/shBrushJScript.js',
+			'html xml site/scripts/syntaxhighlighter/scripts/shBrushXml.js',
+			'css site/scripts/syntaxhighlighter/scripts/shBrushCss.js'
+	);
+	SyntaxHighlighter.all();
+
+	// Init scrollTo
+	$('nav').localScroll({
+		duration : 500,
+		offset   : {top:-70, left:0}
+	});
+
+	// Stylize "caption.js"
+	$("p:contains('caption.js'), h2 a:contains('caption.js')", document.body).contents().each(function() {
+		if (this.nodeType == 3)
+			$(this).replaceWith(this.nodeValue.replace(/caption.js/g, "<span class=\"captionjs\">caption.js</span>"));
+	});
+
+	$("header h1, .captionjs").lettering();
+
+	// Init the sticky nav after content has faded in
+	var position = $('nav').offset();
+	var x_coord = position.top;
+	var padding_to_add = $('nav').outerHeight(true);
+
+	$(window).scroll(function(){
+		var top = $(window).scrollTop();
+		if (top > x_coord - 1)
+		{
+			$('nav').addClass('sticky');
+			$('body > div').css('padding-top', padding_to_add + 'px');
+		}
+		else
+		{
+			$('nav').removeClass('sticky');
+			$('body > div').css('padding-top', '0');
+		}
+	})
+});
