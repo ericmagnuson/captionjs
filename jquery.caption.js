0143f7cc765a7f299e971fffebfc543f8950a4b9
Eric Magnuson <eric@ericmagnuson.me>
2014-03-19 15:37:06 -0600

    Fixes #5: error with images not yet having been painted to screen

--- jquery.caption.js
+++ jquery.caption.js
@@ -2,44 +2,71 @@
  * caption.js | easily and semantically add captions to your images
  * http://captionjs.com
  *
- * Copyright 2013, Eric Magnuson
+ * Copyright 2013-2014, Eric Magnuson
  * Released under the MIT license
  * https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
  *
- * v0.9.3
- * Date: 2013-09-10
+ * v0.9.4 ALPHA
+ * Date: 2014-03-19
  */
 (function($, window, undefined){
 	$.fn.captionjs = function(opts){
+
 		// Default values for options
 		var defaults = {
-			'class_name' : 'captionjs',  // Class name assigned to each <figure>
-			'schema'     : true,         // Use schema.org markup (i.e., itemtype, itemprop)
-			'mode'       : 'default',    // default | static | animated | hide
-			'debug_mode' : false         // Output debug info to the JS console
+			'class_name'      : 'captionjs', // Class name assigned to each <figure>
+			'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
+			'mode'            : 'default',   // default | static | animated | hide
+			'debug_mode'      : false,       // Output debug info to the JS console
+			'force_dimensions': false        // Force the dimensions in case they can't be detected (e.g., image is not yet painted to viewport)
 		}
 
 		// Extend the options from defaults with user's options
 		var options = $.extend(defaults, opts || {});
 
-		// jQuery chainability
+		// jQuery chainability -- do the magic below
 		return this.each(function(){
-			// Do the magic here.
+
 			if (options.debug_mode) console.log('caption.js | Starting.');
 
 			// Form basic structures and assign vars
 			var $this       = $(this),  // The image
-			    $caption    = $this.data('caption') ? $this.data('caption') : $this.attr('alt'),
-			    $figure     = $this.wrap('<figure class="' + options.class_name + '"/>').after('<figcaption/>').parent(),
-			    $figcaption = $this.next('figcaption').html($caption);
+				$caption    = $this.data('caption') ? $this.data('caption') : $this.attr('alt'),
+				$figure     = $this.wrap('<figure class="' + options.class_name + '"/>').after('<figcaption/>').parent(),
+				$figcaption = $this.next('figcaption').html($caption),
+				target_width,
+				target_height;
 
 			// If no caption is supplied, just remove the figcaption.
 			if ($caption === '') $figcaption.remove();
 
 			if (options.debug_mode) console.log('caption.js | Caption: ' + $caption);
 
-			// Set width of the figure, our top-most container for caption.js.
-			$figure.width($this.outerWidth());
+			// Determine the appropriate dimensions for the figure, our top-most container for caption.js.
+			if (options.force_dimensions && ($this.outerWidth() === (undefined || 0)))
+			{
+				if (options.debug_mode) console.log('caption.js | Forcing dimensions with a clone.');
+
+				// Create a clone outside of the viewport to detect and
+				// then set the dimensions.
+				var $clone = $figure.clone().css({
+					'position': 'absolute',
+					'left'    : '-9999px'
+				}).appendTo('body');
+
+				target_width = $('img', $clone).outerWidth(),
+				target_height = $('img', $clone).outerHeight();
+
+				$clone.remove();
+			}
+			else
+			{
+				target_width = $this.outerWidth();
+				target_height = $figcaption.outerHeight();
+			}
+
+			// Set the width of the figure.
+			$figure.width(target_width);
 
 			// Schema markup if enabled
 			if (options.schema === true)
@@ -68,7 +95,7 @@
 				$figure.addClass('animated');
 				$figcaption.css({
 					'margin-bottom': '0',
-					'bottom': -$figcaption.outerHeight(),
+					'bottom': -target_height,
 				});
 			}
 
@@ -77,8 +104,8 @@
 			{
 				$figure.addClass('hide');
 				$figcaption.css({
-					'margin-bottom': $figcaption.outerHeight(),
-					'bottom': -$figcaption.outerHeight(),
+					'margin-bottom': target_height,
+					'bottom': -target_height,
 				});
 			}
 
