9004d6af5d0beb81d09ad81fca7e73f2e6164310
Eric Magnuson <eric@ericmagnuson.me>
2014-03-29 01:38:46 -0600

    Fading in website

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+	// Init SyntaxHighlighter
 	SyntaxHighlighter.autoloader(
-			'js jscript javascript  site/scripts/syntaxhighlighter/scripts/shBrushJScript.js',
+			'js jscript javascript site/scripts/syntaxhighlighter/scripts/shBrushJScript.js',
 			'html xml site/scripts/syntaxhighlighter/scripts/shBrushXml.js',
 			'css site/scripts/syntaxhighlighter/scripts/shBrushCss.js'
 	);
@@ -20,22 +21,35 @@ $(document).ready(function() {
 
 	$("header h1, .captionjs").lettering();
 
-	// Init the sticky nav after content has faded in
-	var position = $('nav').offset();
-	var x_coord = position.top;
-	var padding_to_add = $('nav').outerHeight(true);
+	// Fire these enclosed functions after page has faded in
+	var after_fade_in = function after_fade_in(){
+		// Init the sticky nav
+		var position = $('nav').offset();
+		var x_coord = position.top;
+		var padding_to_add = $('nav').outerHeight(true);
 
-	$(window).scroll(function(){
-		var top = $(window).scrollTop();
-		if (top > x_coord - 1)
-		{
-			$('nav').addClass('sticky');
-			$('body > div').css('padding-top', padding_to_add + 'px');
-		}
-		else
-		{
-			$('nav').removeClass('sticky');
-			$('body > div').css('padding-top', '0');
-		}
-	})
+		$(window).scroll(function(){
+			var top = $(window).scrollTop();
+			if (top > x_coord - 1)
+			{
+				$('nav').addClass('sticky');
+				$('body > div').css('padding-top', padding_to_add + 'px');
+			}
+			else
+			{
+				$('nav').removeClass('sticky');
+				$('body > div').css('padding-top', '0');
+			}
+		})
+	};
+
+	// Fade in content
+	setTimeout(function(){
+		$('.js header h1').fadeIn(1500, function(){
+			$('.js header h2').fadeIn(1000, function(){
+				$('.js #github, .js nav, .js body > div, .js footer').delay(1000).fadeIn(1000, after_fade_in);
+			});
+		});
+	}, 500);
+
 });
