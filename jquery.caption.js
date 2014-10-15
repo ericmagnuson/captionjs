49f7e4cce322141174d02bf7a2145f2152b88f78
Eric Magnuson <eric@ericmagnuson.me>
2014-10-15 13:26:40 -0600

    Added tests page; added style inheritance (still need to do padding)

--- jquery.caption.js
+++ jquery.caption.js
@@ -6,8 +6,8 @@
  * Released under the MIT license
  * https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
  *
- * v0.9.6
- * Date: 2014-09-03
+ * v0.9.7
+ * Date: 2014-10-13
  */
 (function($, window, undefined){
 	$.fn.captionjs = function(opts){
@@ -16,200 +16,24 @@
 		var defaults = {
 			'class_name'      : 'captionjs', // Class name assigned to each <figure>
 			'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
-			'mode'            : 'default',   // default | static | animated | hide
+			'mode'            : 'default',   // default | stacked | animated | hide
 			'debug_mode'      : false,       // Output debug info to the JS console
-			'force_dimensions': false,       // Force the dimensions in case they can't be detected (e.g., image is not yet painted to viewport)
+			'force_dimensions': true,        // Force the dimensions in case they can't be detected (e.g., image is not yet painted to viewport)
 			'is_responsive'   : false,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
-			'inherit_styles'  : true         // Have the caption.js container inherit all styles from the original image
+			'inherit_styles'  : true         // Have the caption.js container inherit all box-model styles from the original image
 		};
 
 		// Extend the options from defaults with user's options
 		var options = $.extend(defaults, opts || {});
 
 		// Function to copy styles
-		(function($){
-			$.fn.getStyleObject = function(){
-				var dom = this.get(0);
-				var style;
-				var returns = {};
-				if(window.getComputedStyle){
-					var camelize = function(a,b){
-						return b.toUpperCase();
-					};
-					style = window.getComputedStyle(dom, null);
-					for(var i = 0, l = style.length; i < l; i++){
-						var prop = style[i];
-						var camel = prop.replace(/\-([a-z])/g, camelize);
-						var val = style.getPropertyValue(prop);
-						returns[camel] = val;
-					};
-					return returns;
-				};
-				if(style = dom.currentStyle){
-					for(var prop in style){
-						returns[prop] = style[prop];
-					};
-					return returns;
-				};
-				return this.css();
+		var transferStyles = function(property, reset_val, $origin, $target){
+			if ($origin.jquery && $target.jquery) // Check that they are jQuery objects
+			{
+				$origin.css(property, $target.css(property));
+				$target.css(property, reset_val);
 			}
-		})(jQuery);
-
-		(function($){
-			$.fn.resetStyles = function(){
-				var resets = {
-					"animation" : "none",
-					"animation-delay" : "0",
-					"animation-direction" : "normal",
-					"animation-duration" : "0",
-					"animation-fill-mode" : "none",
-					"animation-iteration-count" : "1",
-					"animation-name" : "none",
-					"animation-play-state" : "running",
-					"animation-timing-function" : "ease",
-					"backface-visibility" : "visible",
-					"background" : "0",
-					"background-attachment" : "scroll",
-					"background-clip" : "border-box",
-					"background-color" : "transparent",
-					"background-image" : "none",
-					"background-origin" : "padding-box",
-					"background-position" : "0 0",
-					"background-position-x" : "0",
-					"background-position-y" : "0",
-					"background-repeat" : "repeat",
-					"background-size" : "auto auto",
-					"border" : "0",
-					"border-style" : "none",
-					"border-width" : "medium",
-					"border-color" : "inherit",
-					"border-bottom" : "0",
-					"border-bottom-color" : "inherit",
-					"border-bottom-left-radius" : "0",
-					"border-bottom-right-radius" : "0",
-					"border-bottom-style" : "none",
-					"border-bottom-width" : "medium",
-					"border-collapse" : "separate",
-					"border-image" : "none",
-					"border-left" : "0",
-					"border-left-color" : "inherit",
-					"border-left-style" : "none",
-					"border-left-width" : "medium",
-					"border-radius" : "0",
-					"border-right" : "0",
-					"border-right-color" : "inherit",
-					"border-right-style" : "none",
-					"border-right-width" : "medium",
-					"border-spacing" : "0",
-					"border-top" : "0",
-					"border-top-color" : "inherit",
-					"border-top-left-radius" : "0",
-					"border-top-right-radius" : "0",
-					"border-top-style" : "none",
-					"border-top-width" : "medium",
-					"bottom" : "auto",
-					"box-shadow" : "none",
-					"box-sizing" : "content-box",
-					"caption-side" : "top",
-					"clear" : "none",
-					"clip" : "auto",
-					"color" : "inherit",
-					"columns" : "auto",
-					"column-count" : "auto",
-					"column-fill" : "balance",
-					"column-gap" : "normal",
-					"column-rule" : "medium none currentColor",
-					"column-rule-color" : "currentColor",
-					"column-rule-style" : "none",
-					"column-rule-width" : "none",
-					"column-span" : "1",
-					"column-width" : "auto",
-					"content" : "normal",
-					"counter-increment" : "none",
-					"counter-reset" : "none",
-					"cursor" : "auto",
-					"direction" : "ltr",
-					"display" : "inline",
-					"empty-cells" : "show",
-					"float" : "none",
-					"font" : "normal",
-					"font-family" : "inherit",
-					"font-size" : "medium",
-					"font-style" : "normal",
-					"font-variant" : "normal",
-					"font-weight" : "normal",
-					// "height" : "auto",
-					"hyphens" : "none",
-					"left" : "auto",
-					"letter-spacing" : "normal",
-					"line-height" : "normal",
-					"list-style" : "none",
-					"list-style-image" : "none",
-					"list-style-position" : "outside",
-					"list-style-type" : "disc",
-					"margin" : "0",
-					"margin-bottom" : "0",
-					"margin-left" : "0",
-					"margin-right" : "0",
-					"margin-top" : "0",
-					"max-height" : "none",
-					"max-width" : "none",
-					"min-height" : "0",
-					"min-width" : "0",
-					"opacity" : "1",
-					"orphans" : "0",
-					"outline" : "0",
-					"outline-color" : "invert",
-					"outline-style" : "none",
-					"outline-width" : "medium",
-					"overflow" : "visible",
-					"overflow-x" : "visible",
-					"overflow-y" : "visible",
-					"padding" : "0",
-					"padding-bottom" : "0",
-					"padding-left" : "0",
-					"padding-right" : "0",
-					"padding-top" : "0",
-					"page-break-after" : "auto",
-					"page-break-before" : "auto",
-					"page-break-inside" : "auto",
-					"perspective" : "none",
-					"perspective-origin" : "50% 50%",
-					"position" : "static",
-					"quotes" : "'\201C' '\201D' '\2018' '\2019'",
-					"right" : "auto",
-					"tab-size" : "8",
-					"table-layout" : "auto",
-					"text-align" : "inherit",
-					"text-align-last" : "auto",
-					"text-decoration" : "none",
-					"text-decoration-color" : "inherit",
-					"text-decoration-line" : "none",
-					"text-decoration-style" : "solid",
-					"text-indent" : "0",
-					"text-shadow" : "none",
-					"text-transform" : "none",
-					"top" : "auto",
-					"transform" : "none",
-					"transform-style" : "flat",
-					"transition" : "none",
-					"transition-delay" : "0s",
-					"transition-duration" : "0s",
-					"transition-property" : "none",
-					"transition-timing-function" : "ease",
-					"unicode-bidi" : "normal",
-					"vertical-align" : "baseline",
-					"visibility" : "visible",
-					"white-space" : "normal",
-					"widows" : "0",
-					// "width" : "auto",
-					"word-spacing" : "normal",
-					"z-index" : "auto"
-				}
-
-				return this.css(resets);
-			}
-		})(jQuery);
+		};
 
 		// jQuery chainability -- do the magic below
 		return this.each(function(){
@@ -234,15 +58,14 @@
 			{
 				if (options.debug_mode) console.log('caption.js | Forcing dimensions with a clone.');
 
-				// Create a clone outside of the viewport to detect and
-				// then set the dimensions.
+				// Create a clone outside of the viewport to detect and then set the dimensions.
 				var $clone = $figure.clone().css({
 					'position': 'absolute',
 					'left'    : '-9999px'
 				}).appendTo('body');
 
 				target_width = $('img', $clone).outerWidth();
-				target_height = $('figcaption', $clone).css('width', target_width).outerHeight(); // Make sure width (and thus line wrapping) is enforced so that the height is correct
+				target_height = $('figcaption', $clone).css('width', target_width).css('clear', 'both').outerHeight(); // Make sure width (and thus line wrapping) is enforced so that the height is correct
 
 				$clone.remove();
 			}
@@ -263,8 +86,26 @@
 			// Copy styles if need be
 			if (options.inherit_styles)
 			{
-				$figure.css($this.getStyleObject());
-				$this.resetStyles().width('100%');
+				if ($this.css('display') == 'inline')
+					$figure.css('display', 'inline-block');
+				else
+					transferStyles('display', 'block', $figure, $this);
+
+				if ($this.css('position') == 'static')
+					$figure.css('position', 'relative');
+				else
+					transferStyles('position', 'relative', $figure, $this);
+
+				transferStyles('clear', 'both', $figure, $this);
+				transferStyles('float', 'none', $figure, $this);
+				transferStyles('margin', '0', $figure, $this);
+				// transferStyles('padding', '0', $figure, $this);
+				$this.css('padding', '0');
+				transferStyles('left', 'auto', $figure, $this);
+				transferStyles('right', 'auto', $figure, $this);
+				transferStyles('top', 'auto', $figure, $this);
+				transferStyles('bottom', 'auto', $figure, $this);
+				transferStyles('z-index', $this.css('z-index'), $figure, $this);
 			}
 
 			// Set the width of the figure.
